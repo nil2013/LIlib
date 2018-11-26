76fac3ffc46d5791ef2c29a9c2f4774eeed85a2f
Tomoumi NISHIMURA <lue.gsk.2012@gmail.com>
2018-11-26 21:26:19 +0900

    Add level parser
    裁判所の級をテキストから変換できるようにした

--- src/main/scala/me/nsmr/lilib/core/Court.scala
+++ src/main/scala/me/nsmr/lilib/core/Court.scala
@@ -7,6 +7,20 @@ object Court {
   lazy val LEVEL_SHORT = Array("最高", "高", "地", "家", "簡")
 
   def apply(p: String, l: Int, b: String): Court = SimpleCourt(p, l, b)
+
+  /**
+   * 裁判所の級を表すテキストから数値へと変換します。
+   * 該当する級が存在しない場合には-1を返却します。
+   */
+  def levelOf(str: String): Int = {
+    LEVEL.indexOf(str) match {
+      case -1 => LEVEL_SHORT.indexOf(str) match {
+        case -1 => -1
+        case n => n
+      }
+      case n => n
+    }
+  }
 }
 
 trait Court {
