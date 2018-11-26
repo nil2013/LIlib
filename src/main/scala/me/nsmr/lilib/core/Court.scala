a695a579c151beafa1dd52213fac7eb7b159214e
Tomoumi NISHIMURA <lue.gsk.2012@gmail.com>
2018-11-26 17:14:01 +0900

    Fix Court
    裁判所の短縮名を取得できるようにした

--- src/main/scala/me/nsmr/lilib/core/Court.scala
+++ src/main/scala/me/nsmr/lilib/core/Court.scala
@@ -4,6 +4,7 @@ package core
 
 object Court {
   lazy val LEVEL = Array("最高", "高等", "地方", "家庭", "簡易")
+  lazy val LEVEL_SHORT = Array("最高", "高", "地", "家", "簡")
 
   def apply(p: String, l: Int, b: String): Court = SimpleCourt(p, l, b)
 }
@@ -16,6 +17,8 @@ trait Court {
   def branch: String
 
   override def toString(): String = s"$place${LEVEL(level)}裁判所$branch"
+
+  def shortName: String = s"$place${LEVEL_SHORT(level)}裁"
 
   def simpleName: String = s"$place${LEVEL(level)}裁判所"
 }
