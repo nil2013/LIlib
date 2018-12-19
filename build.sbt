94204237de87ea8a9dea63db9ed72371289714e6
nil <lue.gsk.2012@gmail.com>
2018-12-19 11:09:57 +0900

    Add CaseNumberUtil

--- build.sbt
+++ build.sbt
@@ -8,5 +8,9 @@ lazy val root = (project in file(".")).
       version      := "0.1.0-SNAPSHOT"
     )),
     name := "LIlib",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies ++= List(
+      scalaTest % Test,
+      logger.scalaLogging,
+      logger.logback
+    )
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,8 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  object logger {
+    lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
+    lazy val logback      = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  }
 }
