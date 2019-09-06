769f09e5abfb1c2c77b91a863db2053080cd5a39
nil <lue.gsk.2012@gmail.com>
2019-09-06 12:52:51 +0900

    Code Formatting

--- src/main/scala/me/nsmr/lilib/core/CaseNumber.scala
+++ src/main/scala/me/nsmr/lilib/core/CaseNumber.scala
@@ -2,22 +2,26 @@ package me.nsmr
 package lilib
 package core
 
-import java.time.chrono.{ JapaneseChronology, JapaneseDate, JapaneseEra }
+import java.time.chrono.{JapaneseChronology, JapaneseDate, JapaneseEra}
 import java.time.format.DateTimeFormatter
-import java.time.{ LocalDate, Year }
+import java.time.{LocalDate, Year}
 
 object CaseNumber {
   def apply(year: CaseYear, mark: CaseMark, index: Int): CaseNumber = {
     return SimpleCaseNumber(year, mark, index)
   }
+
   def unapply(obj: CaseNumber): Option[(CaseYear, CaseMark, Int)] = return Option((obj.year, obj.mark, obj.index))
 }
 
 trait CaseNumber {
+
   import CaseNumber._
 
   def year: CaseYear
+
   def mark: CaseMark
+
   def index: Int
 
   override def toString(): String = s"${year}${mark}第${index}号"
@@ -30,26 +34,34 @@ object CaseYear {
   }
 
   object Era {
+
     case object Meiji extends Era("明治", JapaneseEra.MEIJI)
+
     case object Taisho extends Era("大正", JapaneseEra.TAISHO)
+
     case object Showa extends Era("昭和", JapaneseEra.SHOWA)
+
     case object Heisei extends Era("平成", JapaneseEra.HEISEI)
 
     def M = Meiji
+
     def T = Taisho
+
     def S = Showa
+
     def H = Heisei
 
     def list: Array[Era] = Array(M, T, S, H)
 
     def apply(idx: Int): Era = list(idx)
+
     def apply(javaEra: JapaneseEra): Era = javaEra match {
       case JapaneseEra.HEISEI => this.Heisei
       case JapaneseEra.SHOWA => this.Showa
       case JapaneseEra.TAISHO => this.Taisho
       case JapaneseEra.MEIJI => this.Meiji
     }
-    
+
     def apply(str: String): Option[Era] = list.find(_.name == str)
   }
 
@@ -90,6 +102,7 @@ object CaseMark {
 case class CaseMark(prefix: Option[Char], mark: String, suffix: Option[Char]) {
 
   override def toString = s"${prefix.mkString}(${mark})${suffix.mkString}"
+
   // override def toString = s"CaseMark(prefix = '${prefix.mkString}', mark = '${mark}', suffix = '${suffix.mkString}')"
 
   def hasPrefix: Boolean = this.prefix.isDefined
@@ -99,14 +112,14 @@ case class CaseMark(prefix: Option[Char], mark: String, suffix: Option[Char]) {
   def withPrefix(prefix: Char): CaseMark = this.copy(prefix = Option(prefix))
 
   def withPrefix(prefix: String): CaseMark = {
-    if(prefix.length == 1) this.withPrefix(prefix.charAt(0))
+    if (prefix.length == 1) this.withPrefix(prefix.charAt(0))
     else throw new IllegalArgumentException("prefix must be just 1 character.")
   }
 
   def withSuffix(suffix: Char): CaseMark = this.copy(suffix = Option(suffix))
 
   def withSuffix(suffix: String): CaseMark = {
-    if(suffix.length == 1) this.withSuffix(suffix.charAt(0))
+    if (suffix.length == 1) this.withSuffix(suffix.charAt(0))
     else throw new IllegalArgumentException("suffix must be just 1 character.")
   }
 
